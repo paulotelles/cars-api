bdeee475695205bd28bc92e6d859d66764fc6697
Paulo Marques <paulo.marques.souza@accenture.com>
2022-03-08 06:33:27 -0300

    docs: implement swagger module and build docs page

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,5 @@
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
 import { AppModule } from './app.module';
 import { NestFactory } from '@nestjs/core';
 
@@ -5,6 +7,15 @@ const PORT = process.env.PORT || 3000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Cars API')
+    .setDescription('This API was developped as part of a test for Cognigy')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
   await app.listen(PORT);
 }
 bootstrap();
