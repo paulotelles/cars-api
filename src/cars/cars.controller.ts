d78d69d8f7857bee5eba430a787ba13b1b03beee
Paulo Marques <paulo.marques.souza@accenture.com>
2022-03-09 12:15:44 -0300

    feat: includding pipe for validate payload

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -1,12 +1,24 @@
-import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
-import { Car } from './model/cars.model';
+import {
+  Body,
+  Controller,
+  HttpCode,
+  HttpStatus,
+  Inject,
+  Post,
+  UsePipes,
+} from '@nestjs/common';
+import { AvjValidationPipe } from '../pipes/ajv-validation.pipes';
+import { CarsService } from './cars.service';
+import { Car, CarDocument } from './model/cars.model';
+import { carJsonSchema } from './schema/cars.schema';
 
 @Controller('cars')
 export class CarsController {
-  constructor() {}
+  constructor(@Inject(CarsService) private carService: CarsService) {}
   @Post()
+  @UsePipes(new AvjValidationPipe(carJsonSchema))
   @HttpCode(HttpStatus.CREATED)
-  async createCar(@Body() body: Car) {
-    return 'test';
+  async createCar(@Body() body: Car): Promise<CarDocument> {
+    return this.carService.create(body);
   }
 }
---
+++ src/pipes/ajv-validation.pipes.ts
@@ -0,0 +1,29 @@
+import Ajv, { Schema } from 'ajv';
+import {
+  ArgumentMetadata,
+  BadRequestException,
+  Injectable,
+  PipeTransform,
+} from '@nestjs/common';
+
+@Injectable()
+export class AvjValidationPipe implements PipeTransform {
+  constructor(private schema: Schema) {}
+
+  transform(value: any, metadata: ArgumentMetadata) {
+    const ajv = new Ajv({ allErrors: true });
+    const validate = ajv.compile(this.schema);
+    const valid = validate(value);
+    if (!valid) {
+      console.log(validate.errors);
+      const message = validate.errors?.map((error) => {
+        return {
+          field: error.instancePath.substring(1),
+          message: error.message,
+        };
+      });
+      throw new BadRequestException(message);
+    }
+    return value;
+  }
+}
